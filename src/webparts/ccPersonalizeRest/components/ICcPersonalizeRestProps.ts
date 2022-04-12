c9fe41fc6f381cfcd95490bfc1487175069dcce7
Noel Suarez <suarez@chapman.com>
2022-04-12 11:18:00 -0500

    initial

---
+++ src/webparts/ccPersonalizeRest/components/ICcPersonalizeRestProps.ts
@@ -0,0 +1,7 @@
+export interface ICcPersonalizeRestProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
